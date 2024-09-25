f3102c9c4cc3b440239ed37aa2cb2347447d7427
vinhtran113 <tvih6693@gmail.com>
2024-09-26 01:15:02 +0700

    bài tập tuần 05

---
+++ fitness_workout_app/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:fitness_workout_app/view/main_tab/main_tab_view.dart';
+import 'package:fitness_workout_app/view/on_boarding/started_view.dart';
+import 'package:fitness_workout_app/view/login/login_view.dart';
+import 'package:fitness_workout_app/view/login/signup_view.dart';
+import 'package:fitness_workout_app/view/home/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fitness_workout_app/view/login/complete_profile_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'common/colo_extension.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug, // Sử dụng token mặc định trên Android
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitness 3 in 1',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: TColor.primaryColor1,
+        fontFamily: "Poppins",
+      ),
+      // Thiết lập route mặc định
+      initialRoute: '/start', // Trang khởi đầu là trang StartedView
+      // Cấu hình các routes
+      routes: {
+        '/start': (context) => const StartedView(),
+        '/login': (context) => const LoginView(),
+        '/signup': (context) => const SignUpView(),
+        '/home': (context) => const HomeView(),
+        '/main': (context) => const MainTabView(), // Màn hình chính có tab
+        '/completeProfile': (context) => const CompleteProfileView(),
+
+      },
+    );
+  }
+}
---
+++ fitness_workout_app/lib/view/login/login_view.dart
@@ -0,0 +1,261 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitness_workout_app/common/colo_extension.dart';
+import 'package:fitness_workout_app/common_widget/round_button.dart';
+import 'package:fitness_workout_app/common_widget/round_textfield.dart';
+import 'package:fitness_workout_app/view/login/complete_profile_view.dart';
+import 'package:flutter/material.dart';
+import 'package:fitness_workout_app/services/auth.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isCheck = false;
+
+  final _auth = AuthService();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+  // email and passowrd auth part
+  void handleLogin() async {
+    setState(() {
+      isCheck = true;
+    });
+    // signup user using our authmethod
+    String res = await AuthService().loginUser(
+        email: emailController.text, password: passwordController.text);
+
+    if (res == "success") {
+      setState(() {
+        isCheck = false;
+      });
+      //navigate to the home screen
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      setState(() {
+        isCheck = false;
+      });
+      // show error
+      const SnackBar(content: Text('Đăng nhập thất bại, kiểm tra thông tin!'));
+    }
+  }
+/*
+  void handleLogin() async {
+    User? user = await _authService.login(
+      emailController.text.trim(),
+      passwordController.text.trim(),
+    );
+
+    if (user == null) {
+      // Nếu có lỗi, hiện thông báo
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Đăng nhập thất bại, kiểm tra thông tin!')),
+      );
+    } else {
+      // Đăng nhập thành công, chuyển trang
+      Navigator.pushReplacementNamed(context, '/home');
+    }
+  }
+
+*/
+
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: TColor.white,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            height: media.height * 0.9,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Hey there,",
+                  style: TextStyle(color: TColor.gray, fontSize: 16),
+                ),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                      color: TColor.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  height: media.width * 0.05,
+                ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+                RoundTextField(
+                  hitText: "Email",
+                  icon: "assets/img/email.png",
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+                RoundTextField(
+                  hitText: "Password",
+                  icon: "assets/img/lock.png",
+                  controller: passwordController,
+                  obscureText: true,
+                  rigtIcon: TextButton(
+                      onPressed: () {},
+                      child: Container(
+                          alignment: Alignment.center,
+                          width: 20,
+                          height: 20,
+                          child: Image.asset(
+                            "assets/img/show_password.png",
+                            width: 20,
+                            height: 20,
+                            fit: BoxFit.contain,
+                            color: TColor.gray,
+                          ))),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Forgot your password?",
+                      style: TextStyle(
+                          color: TColor.gray,
+                          fontSize: 10,
+                          decoration: TextDecoration.underline),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                RoundButton(
+                    title: "Login",
+                    onPressed: handleLogin
+                    ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+                Row(
+                  // crossAxisAlignment: CrossAxisAlignment.,
+                  children: [
+                    Expanded(
+                        child: Container(
+                          height: 1,
+                          color: TColor.gray.withOpacity(0.5),
+                        )),
+                    Text(
+                      "  Or  ",
+                      style: TextStyle(color: TColor.black, fontSize: 12),
+                    ),
+                    Expanded(
+                        child: Container(
+                          height: 1,
+                          color: TColor.gray.withOpacity(0.5),
+                        )),
+                  ],
+                ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: TColor.white,
+                          border: Border.all(
+                            width: 1,
+                            color: TColor.gray.withOpacity(0.4),
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Image.asset(
+                          "assets/img/google.png",
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: media.width * 0.04,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: TColor.white,
+                          border: Border.all(
+                            width: 1,
+                            color: TColor.gray.withOpacity(0.4),
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Image.asset(
+                          "assets/img/facebook.png",
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Don’t have an account yet? ",
+                        style: TextStyle(
+                          color: TColor.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        "Register",
+                        style: TextStyle(
+                            color: TColor.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: media.width * 0.04,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
